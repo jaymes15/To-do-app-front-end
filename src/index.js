771a27f6f2f158404cb871758477c72d6d79750d
james tomisin <jamesoluwatomisinomotosho@gmail.com>
2019-11-01 21:29:24 +0400

    Intergrate post_request to backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 
 class ContentFeed extends React.Component{
@@ -7,9 +8,13 @@ class ContentFeed extends React.Component{
     	super(props);
     	this.state ={
     		items : [],
-    		oneitem:''
+    		oneitem:'',
+    		title: null,
+    		description:null
     	}
     	this.getoneItem = this.getoneItem.bind(this);
+    	this.myChangeHandler = this.myChangeHandler.bind(this);
+    	this.mySubmitHandler = this.mySubmitHandler.bind(this);
     };
 	componentDidMount(){
 		this.getItems();
@@ -29,10 +34,53 @@ class ContentFeed extends React.Component{
 
 	};
 
+	myChangeHandler(event){
+		let nam = event.target.name;
+    	let val = event.target.value;
+    	
+    	this.setState({[nam]: val});
+
+	};
+
+   mySubmitHandler = (event) => {
+    	event.preventDefault();
+    	console.log(this.state.title);
+    	 $.post('http://127.0.0.1:8000/api/item/',
+				  {
+				    title: this.state.title,
+				    description: this.state.description
+				  },
+				  function(data, status){
+				    alert("Data: " + data + "\nStatus: " + status);
+				   
+				  });
+    	  fetch('http://127.0.0.1:8000/api/item/')
+		  .then(results => results.json())
+		  .then(results => this.setState({ items : results }));
+		  
+	}
 
 	render(){
 		return (
 			<div>
+
+
+				 <form onSubmit={this.mySubmitHandler}>
+				      
+				      <p>Title:</p>
+				      <input
+				        type='text'
+				        name='title'
+				        onChange={this.myChangeHandler}
+				      />
+				      <p>Description:</p>
+				      <input
+				        type='text'
+				        name='description'
+				        onChange={this.myChangeHandler}
+				      />
+				      <input type='submit'/>
+				</form>
 				<h1>{this.state.oneitem.id}</h1>
 				<h1>{this.state.oneitem.title}</h1>
 				<h1>{this.state.oneitem.description}</h1>
